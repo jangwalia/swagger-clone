62385c85ed109acb987b6bfe0ba3394da6546738
jang walia <jangiwalia83@gmail.com>
2022-07-04 14:03:33 -0700

    refactor serverless file

--- api/pets.js
+++ api/pets.js
@@ -1,13 +1,41 @@
 'use strict';
-module.exports.create = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
- 
-  callback(null, response);
- 
+import { v4 as uuidv4 } from 'uuid';
+const AWS = require('aws-sdk'); 
+	
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+//helper function
+
+function response(statusCode, message){
+  return{
+    statusCode,
+    body: JSON.stringify(message)
+  }
+}
+
+module.exports.createPet = (event, context, callback) => {
+  const reqBody = JSON.parse(event.body);
+
+ const params = {
+    TableName: "pets",
+    Item: {
+      id: uuidv4(),
+      name: reqBody.name,
+      tags: reqBody.tags,
+      category: reqBody.category
+    }
+ };
+
+ dynamoDb.put(params,(err,data) => {
+    if(err){
+      console.log(err);
+      callback(new Error(err));
+      return;
+    }
+    const response = {
+      statusCode: 201,
+      body: JSON.stringify(data.Item)
+    };
+    callback(null,response)
+ })
 };
