0b99699f71db928a37c5737e87c803e076d2f15a
jang walia <jangiwalia83@gmail.com>
2022-07-04 18:54:39 -0700

    implement DeleteItem function

--- api.js
+++ api.js
@@ -102,3 +102,31 @@ const UpdatePet = async (event)=>{
 
   return response;
 }
+
+
+//**********Delete ITEM FUNCTION */
+
+const deletePet = async (event)=>{
+  const response = {statusCode: 200}
+  try {
+   const params = {
+      TableName : process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({petId: event.pathParameters.petId}),
+     
+    };
+    const deletedResult = await db.send(new DeleteItemCommand(params))
+    response.body = JSON.stringify({
+      message: "Successfully removed data",
+      deletedResult
+    })
+  } catch (error) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "failed to remove data",
+      errorMsg : error.message,
+      errorStack: error.Stack
+    })
+  }
+
+  return response;
+}
