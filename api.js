394594fe736e48a4c7940abb8032d92e67daeee9
jang walia <jangiwalia83@gmail.com>
2022-07-04 18:47:52 -0700

    implemented update pet function

--- api.js
+++ api.js
@@ -64,3 +64,41 @@ const createPet = async (event)=>{
 
   return response;
 }
+
+
+//**********Update ITEM FUNCTION */
+
+const UpdatePet = async (event)=>{
+  const response = {statusCode: 200}
+  try {
+    const body = JSON.parse(event.body)
+    const objKeys = Object.keys(body)
+    const params = {
+      TableName : process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({petId: event.pathParameters.petId}),
+      UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+            ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`#key${index}`]: key,
+            }), {}),
+            ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`:value${index}`]: body[key],
+            }), {})),
+    };
+    const updateResult = await db.send(new UpdateItemCommand(params))
+    response.body = JSON.stringify({
+      message: "Successfully updated data",
+      updateResult
+    })
+  } catch (error) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "failed to update data",
+      errorMsg : error.message,
+      errorStack: error.Stack
+    })
+  }
+
+  return response;
+}
